acd7e993f454fa44520db01fcaeefce64a34888d
MariR <mariliza989@mail.ru>
2023-05-08 16:46:11 +0300

    Добавлена обработка ошибок, улучшен вывод программы

--- main.py
+++ main.py
@@ -6,19 +6,35 @@ import face_detection
 
 # Загружает фотографии в виде массива numpy
 def load_photos():
-    face = cv.imread("Source/guy3.jpeg")
-    passport = cv.imread("Source/pass_great.jpeg")
-    return face, passport
+    face = None
+    passport = None
+    try:
+        face = cv.imread("Source/Pretty_girl.jpg")
+        passport = cv.imread("Source/pass_another.jpg")
+    finally:
+        return face, passport
 
 
 def main():
     face, passport = load_photos()
+    if face is None or passport is None:
+        print("-1\nОшибка загрузки изображений")
+
     faces_checked = face_detection.process_faces(face, passport)
+    if faces_checked is None:
+        print("-2\nОшибка распознавания лиц")
+        return
+
     if faces_checked:
         age = passport_recognition.get_age("Source/Temp/passport_bottom.jpg")
-        print(age)
+        if age is None:
+            print("-3\nОшибка распознавания возраста")
+        elif age >= 18:
+            print("0\nПроверка пройдена успешно")
+        else:
+            print("1\nПроверка не пройдена - возраст меньше 18")
     else:
-        print("Failed")
+        print("2\nПроверка не пройдена - лица не совпадают")
 
 
 if __name__ == "__main__":
--- passport_recognition.py
+++ passport_recognition.py
@@ -39,43 +39,46 @@ def get_text_from_passport(img_path,
     :param psm_mode: Режим страничной сегментации pytesseract
     :return: весь текст, найденный на фотографии
     """
-    image = cv.imread(img_path)
+    try:
+        image = cv.imread(img_path)
 
-    if resize_coefficient != 1.0:
-        image = resize_image(image, resize_coefficient)
+        if resize_coefficient != 1.0:
+            image = resize_image(image, resize_coefficient)
 
-    # преобразование в оттенки серого
-    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        # преобразование в оттенки серого
+        image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
-    # небольшой блюр для уменьшения шума
-    if use_blur:
-        image = cv.GaussianBlur(image, (3, 3), 0)
+        # небольшой блюр для уменьшения шума
+        if use_blur:
+            image = cv.GaussianBlur(image, (3, 3), 0)
 
-    # преобразование цвета в бинарный на основе алгоритма OTSU
-    image = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+        # преобразование цвета в бинарный на основе алгоритма OTSU
+        image = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
 
-    # Морфологическое преобразования
-    if use_morph:
-        kernel = cv.getStructuringElement(kernel_shape, (kernel_size, kernel_size))
-        image = cv.morphologyEx(image, morph_type, kernel)
+        # Морфологическое преобразования
+        if use_morph:
+            kernel = cv.getStructuringElement(kernel_shape, (kernel_size, kernel_size))
+            image = cv.morphologyEx(image, morph_type, kernel)
 
-    # Извлечение текста
-    data = pytesseract.image_to_string(image, config=f'-l eng --psm 6 {psm_mode}')
-    return data
+        # Извлечение текста
+        data = pytesseract.image_to_string(image, config=f'-l eng --psm 6 {psm_mode}')
+        return data
+    except Exception:
+        return None
 
 
 # Распознает дату рождения из текста паспорта и высчитывает возраст
 def extract_age(text):
-    mrz_start = "pnrus"  # начало МЧЗ
+    mrz_start = "pn"  # начало МЧЗ
     birthdate_start = "rus"  # начало строки с датой рождения
 
     # преобразование исходного текста
-    text = text.lower().replace(' ', '')
+    text = text.lower().replace(' ', '').replace('o', '0')
     text = text.split('\n')
 
     text_len = len(text)  # длина листа строк
     line = -1  # текущая строка текста
-    age = -1  # возраст
+    age = None  # возраст
 
     # поиск МЧЗ
     for i in range(len(text)):
@@ -85,9 +88,9 @@ def extract_age(text):
 
     # если line остался -1, МЧЗ не найдена
     if line == -1:
-        return -1
+        return age
 
-    if (line+1) < text_len:
+    try:
         # определение даты рождения
         index = text[line+1].find(birthdate_start)+3
         birthday_str = text[line+1][index:index+6]
@@ -95,12 +98,14 @@ def extract_age(text):
         birthday = datetime.strptime(birthday_str, "%y%m%d")
         # расчет возраста
         age = relativedelta(datetime.now(), birthday).years
-
-    return age
+    finally:
+        return age
 
 
 # принимает фото паспорта (МЧЗ паспорта) и возвращает возраст владельца
 def get_age(img_path):
     passport_text = get_text_from_passport(img_path)
-    age = extract_age(passport_text)
+    age = None
+    if passport_text is not None:
+        age = extract_age(passport_text)
     return age
