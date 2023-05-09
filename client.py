81961e07eee5f7f5d01d4d1b2f7bd05e0ad89edf
MariR <mariliza989@mail.ru>
2023-05-09 15:58:27 +0300

    Перенесены функции приложения в файл client.py

---
+++ client.py
@@ -0,0 +1,44 @@
+import passport_recognition
+import face_detection
+import settings
+
+import cv2 as cv
+
+
+# Загружает фотографии в виде массива numpy
+def load_photos(face_path, passport_path):
+    try:
+        face = cv.imread(face_path)
+        passport = cv.imread(passport_path)
+        return face, passport
+    except Exception:
+        return None, None
+
+
+def start_job():
+    cfg = settings.Settings('settings.ini')
+    path = cfg.get('PATH')
+    text_rec = cfg.get('Text Recognition')
+    output = cfg.get('Output')
+
+    face, passport = load_photos(path['source_face_photo'], path['source_passport_photo'])
+
+    if (face is None) or (passport is None):
+        print(output['error_loading_images'])
+        return
+
+    faces_checked = face_detection.process_faces(path['temp_folder'], face, passport)
+    if faces_checked is None:
+        print(output['error_face_recognition'])
+        return
+
+    if faces_checked:
+        age = passport_recognition.get_age(text_rec, path['temp_folder'])
+        if age is None:
+            print(output['error_age_extraction'])
+        elif age >= 18:
+            print(output['result_success'])
+        else:
+            print(output['result_bad_age'])
+    else:
+        print(output['result_bad_faces'])
--- main.py
+++ main.py
@@ -1,47 +1,8 @@
-import cv2 as cv
-
-import passport_recognition
-import face_detection
-import settings
-
-
-# Загружает фотографии в виде массива numpy
-def load_photos(face_path, passport_path):
-    try:
-        face = cv.imread(face_path)
-        passport = cv.imread(passport_path)
-        return face, passport
-    except Exception:
-        return None, None
+import client
 
 
 def main():
-    cfg = settings.Settings('settings.ini')
-    path = cfg.get('PATH')
-    text_rec = cfg.get('Text Recognition')
-    output = cfg.get('Output')
-
-    face, passport = load_photos(path['source_face_photo'], path['source_passport_photo'])
-
-    if (face is None) or (passport is None):
-        print(output['error_loading_images'])
-        return
-
-    faces_checked = face_detection.process_faces(path['temp_folder'], face, passport)
-    if faces_checked is None:
-        print(output['error_face_recognition'])
-        return
-
-    if faces_checked:
-        age = passport_recognition.get_age(text_rec, path['temp_folder'])
-        if age is None:
-            print(output['error_age_extraction'])
-        elif age >= 18:
-            print(output['result_success'])
-        else:
-            print(output['result_bad_age'])
-    else:
-        print(output['result_bad_faces'])
+    client.start_job()
 
 
 if __name__ == "__main__":
