245e14b02d650a0f58cd64179861970295cc660a
MariR <mariliza989@mail.ru>
2023-05-10 18:16:50 +0300

    Добавлены комментарии

--- Worker/worker.py
+++ Worker/worker.py
@@ -1,26 +1,35 @@
+# Импорт модулей
 from Worker import passport_recognition
 from Worker import face_detection
 
 
+# Основная функция модуля принятия решений
 def start_job(cfg, face, passport):
+
+    # Извлечение настроек
     path = cfg.get('PATH')
     text_rec = cfg.get('Text Recognition')
     output = cfg.get('Output')
 
+    # Проверка фотографий на правильность считывания
     if (face is None) or (passport is None):
         return output['error_loading_images']
 
+    # Сравнение лиц
     faces_checked = face_detection.process_faces(path['temp_folder'], face, passport)
+    # Обработка ошибок сравнения лиц
     if faces_checked is None:
         return output['error_face_recognition']
 
+    # Если лица совпадают, анализ паспорта
     if faces_checked:
         age = passport_recognition.get_age(text_rec, path['temp_folder'])
-        if age is None:
+        # Обработка результата анализа паспорта
+        if age is None:  # Ошибка обработки паспорта
             return output['error_age_extraction']
-        elif age >= 18:
+        elif age >= 18:  # Возраст больше 18
             return output['result_success']
-        else:
+        else:  # Возраст меньше 18
             return output['result_bad_age']
-    else:
+    else: # Если лица не совпадают
         return output['result_bad_faces']
